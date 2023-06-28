60bbf4f87fca77cdf888f9ed6881c696c3e41c49
Scott Rod <djmtype@gmail.com>
2023-06-28 12:09:41 -0400

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        icon({
+            include: {
+                ic: ['*'],
+                fe: ['*'],
+                ri: ['*'],
+                bi: ['*'],
+            }
+        })
+    ]
+});
